836fdb4c232ee9cc6e7c15c6675ed1c7cdfdbec4
codersharif <khansharifulislam24@gmail.com>
2024-01-15 03:51:21 +0600

    Rendering Lists

--- README.md
+++ README.md
@@ -7,8 +7,6 @@ Currently, two official plugins are available:
 - [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
 - [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
 
-# Passing Props to a Component
+# Rendering Lists
 
-# Forwarding props with the JSX spread syntax
-
-# Passing JSX as children
+https://react.dev/learn/rendering-lists
--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,65 @@
-import Card from "./components/Card";
-import Profile from "./components/Profile";
+const people = [
+  {
+    id: 0,
+    name: "Creola Katherine Johnson",
+    profession: "mathematician",
+  },
+  {
+    id: 1,
+    name: "Mario José Molina-Pasquel Henríquez",
+    profession: "chemist",
+  },
+  {
+    id: 2,
+    name: "Mohammad Abdus Salam",
+    profession: "physicist",
+  },
+  {
+    id: 3,
+    name: "Percy Lavon Julian",
+    profession: "chemist",
+  },
+  {
+    id: 4,
+    name: "Subrahmanyan Chandrasekhar",
+    profession: "astrophysicist",
+  },
+];
 
-// assing Props to a Component
-// Forwarding props with the JSX spread synta
-{
-  /* 
-  function App() {
-    return (
-      <>
-        <Profile
-          person={{ name: "Sharif", age: "25" }}
-          color={{ color: "red" }}
-        />
-      </>
-    );
-  }
-  
-  */
-}
-
-//Passing JSX as children
 function App() {
+  // const chemist = people
+  //   .filter((person) => person.profession === "chemist")
+  //   .map((person) => <li key={person.id}>{person.name}</li>);
+  const chemist = people.filter((person) => person.profession === "chemist");
+
+  const listItem = chemist.map((person) => (
+    <li key={person.id}>{person.name}</li>
+  ));
+
   return (
-    <>
-      <Card>
-        <Profile
-          person={{ name: "React", age: "12" }}
-          color={{ color: "red" }}
-        />
-      </Card>
-    </>
+    <div>
+      <ul>{listItem}</ul>
+    </div>
   );
 }
+
+// const poem = {
+//   lines: [
+//     "I write, erase, rewrite",
+//     "Erase again, and then",
+//     "A poppy blooms.",
+//   ],
+// };
+
+// function App() {
+//   let output = [];
+
+//   poem.lines.forEach((line, i) => {
+//     output.push(<hr key={i + "-separator"} />);
+//     output.push(<p key={i + "-test"}>{line}</p>);
+//   });
+//   output.shift();
+//   return <article>{output}</article>;
+// }
 
 export default App;
