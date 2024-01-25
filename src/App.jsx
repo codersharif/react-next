2692389b9ea4c0ea45b5b91b2aae770f606078ab
codersharif <khansharifulislam24@gmail.com>
2024-01-25 08:42:23 +0600

    Form -Add input & pointer-move

--- src/App.jsx
+++ src/App.jsx
@@ -1,65 +1,12 @@
-const people = [
-  {
-    id: 0,
-    name: "Creola Katherine Johnson",
-    profession: "mathematician",
-  },
-  {
-    id: 1,
-    name: "Mario José Molina-Pasquel Henríquez",
-    profession: "chemist",
-  },
-  {
-    id: 2,
-    name: "Mohammad Abdus Salam",
-    profession: "physicist",
-  },
-  {
-    id: 3,
-    name: "Percy Lavon Julian",
-    profession: "chemist",
-  },
-  {
-    id: 4,
-    name: "Subrahmanyan Chandrasekhar",
-    profession: "astrophysicist",
-  },
-];
+import FormAddInput from "./components/FormAddInput";
 
 function App() {
-  // const chemist = people
-  //   .filter((person) => person.profession === "chemist")
-  //   .map((person) => <li key={person.id}>{person.name}</li>);
-  const chemist = people.filter((person) => person.profession === "chemist");
-
-  const listItem = chemist.map((person) => (
-    <li key={person.id}>{person.name}</li>
-  ));
-
   return (
     <div>
-      <ul>{listItem}</ul>
+      {/* <PointerTest /> */}
+      <FormAddInput />
     </div>
   );
 }
-
-// const poem = {
-//   lines: [
-//     "I write, erase, rewrite",
-//     "Erase again, and then",
-//     "A poppy blooms.",
-//   ],
-// };
-
-// function App() {
-//   let output = [];
-
-//   poem.lines.forEach((line, i) => {
-//     output.push(<hr key={i + "-separator"} />);
-//     output.push(<p key={i + "-test"}>{line}</p>);
-//   });
-//   output.shift();
-//   return <article>{output}</article>;
-// }
 
 export default App;
